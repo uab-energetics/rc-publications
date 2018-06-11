76a6b263611ff462b37d7458cdd977e868c2e9d1
Caleb <caleb.falcione@gmail.com>
2018-06-11 14:57:22 -0500

    fixes publication schema

--- src/repositories/models/publication.schema.ts
+++ src/repositories/models/publication.schema.ts
@@ -3,6 +3,6 @@ const joi = require('joi')
 export const PublicationSchema = {
     embeddingURL: joi.string().required(),
     title: joi.string().required(),
-    sourceID: joi.string(),
+    sourceID: joi.string().allow('', null),
     uuid: joi.string()
 }
